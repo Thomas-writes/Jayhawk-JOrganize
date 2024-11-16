4d9d70d1ebf62d305d3ad064b7a91658dc0016f5
Thomas Savasten <thomas@savasten.com>
2024-11-16 14:49:43 -0600

    html work

--- test.py
+++ test.py
@@ -1,7 +1,15 @@
-from flask import Flask
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
 @app.route("/")
 def main():
-    return "<p>Hello, World!</p>"
+    return render_template('data_analyzer.html')
+
+def get_info():
+    data = request.get_json()
+    URL = data.get('value')
+    print(f"{URL}")
+
+if __name__ == '__main__':
+    app.run()
