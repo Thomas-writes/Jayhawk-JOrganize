1b6b5de5caf1cd55c5905ca73ce7ca47f70ae287
Thomas Savasten <thomas@savasten.com>
2024-11-16 18:39:31 -0600

    should be good

--- test.py
+++ test.py
@@ -1,5 +1,7 @@
 from flask import Flask, render_template, request, jsonify
 from flask_cors import CORS
+import subprocess
+
 
 app = Flask(__name__)
 CORS(app)
@@ -10,12 +12,20 @@ def main():
 
 @app.route('/store', methods=['POST'])
 def store():
+    global stored_url
     data = request.get_json()
     URL = data.get('value')
     URL = '"' +  URL + '"'
     print(URL)
-    return jsonify("message: URL")
+    stored_url = URL
+    result = subprocess.run(['python3', 'test2.py', URL], capture_output=True, text=True)
+    print(result.stdout.strip('\n'))
+    return jsonify(message=stored_url)
 
+@app.route('/get_url')
+def get_url():
+    return jsonify(url=stored_url)
 
 if __name__ == '__main__':
     app.run(debug=True)
+
---
+++ test2.py
@@ -0,0 +1,8 @@
+import sys
+
+def main():
+    print("test2")
+    url = sys.argv[1]
+    print(url)
+    
+main()
